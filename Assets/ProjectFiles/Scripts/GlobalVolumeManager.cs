d85f552def2653656170de0d1f4e530569b60211
tomasspano <tomispano05@gmail.com>
2026-06-13 20:15:05 +0200

    global volume, item imports, retoques scripts

---
+++ Assets/ProjectFiles/Scripts/GlobalVolumeManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class GlobalVolumeManager : MonoBehaviour
+{
+    public static GlobalVolumeManager Instance;
+    
+    private Volume m_volume;
+    private ColorAdjustments m_colAdjust;
+    private ChromaticAberration m_aberration;
+    
+    
+}
--- Assets/ProjectFiles/Scripts/HitObjects/DestructibleObject.cs
+++ Assets/ProjectFiles/Scripts/HitObjects/DestructibleObject.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,9 +7,17 @@ public class DestructibleObject : MonoBehaviour, IHittableGameObjectByPlayer
 {
     public GameObject m_item;
     public Transform  m_pivot;
+    private SpriteRenderer m_spriteRenderer;
+    public Sprite m_hitSprite;
+
+    private void Awake()
+    {
+        m_spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     public void HitByPlayer(float damage, CharacterBeatController player)
     {
+        m_spriteRenderer.sprite = m_hitSprite;
         Instantiate(m_item, m_pivot.position, Quaternion.identity);
         Destroy(gameObject);
     }
