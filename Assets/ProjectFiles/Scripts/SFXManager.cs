49a0907d41c3420cfd63003aa2577011d977574e
tomasspano <tomispano05@gmail.com>
2026-06-14 16:50:36 +0200

    org carpetas, imports y sfx enum

---
+++ Assets/ProjectFiles/Scripts/SFXManager.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace ProjectFiles.Scripts
+{
+    public class SFXManager : MonoBehaviour
+    {
+        
+    }
+}
---
+++ Assets/ProjectFiles/Scripts/SFXType.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public enum SFXType
+{
+    //player
+    PlayerPunch,
+    PlayerSword,
+    PlayerHurt,
+    PlayerDeath,
+    
+    //enemies
+    EnemyPunch,
+    EnemyHurt,
+    EnemyDeath,
+    
+    //boss
+    BossHit,
+    BossHurt,
+    BossLaugh,
+    BossDeath,
+    
+    //items
+    ItemHeal,
+    ItemFreeze,
+    
+    //UI
+    UIConfirm,
+    UICancel
+}
